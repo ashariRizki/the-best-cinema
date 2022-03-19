11461e5463a0a79b490a7f9a1db0bcce68653a29
rizkiashari <rizkiashari1026@gmail.com>
2022-03-19 20:26:08 +0700

    Axios

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export interface CardProps {
+  title: string;
+  poster_path: string;
+  release_date: string;
+  vote_average: number;
+}
+
+const Card: React.FC<CardProps> = (props) => {
+  return (
+    <div>
+      <h1>{props.title}</h1>
+      <img src={props.poster_path} alt={props.title} />
+      <p>{props.release_date}</p>
+    </div>
+  );
+};
+
+export default Card;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,30 @@
 import React, { useEffect } from "react";
+import Card from "../../components/Card";
 import Header from "../../components/Header";
+import { API } from "../../config/api";
 
 const HomePage: React.FC = () => {
+  const [movies, setMovies] = React.useState<any[]>([]);
+
+  const getMovies = async () => {
+    const res = await API.get(
+      `/movie/popular?api_key=779ac0f172ef3f22c3585a35ed6c047f&language=en-US&page=1`
+    );
+
+    console.log(res);
+  };
+
   useEffect(() => {
     document.title = "The Best Cinema | Home";
+
+    getMovies();
   }, []);
+
   return (
-    <div>
+    <>
       <Header />
-    </div>
+      <div className='mx-auto flex max-w-[1400px] flex-wrap mt-4'>{/* <Card /> */}</div>
+    </>
   );
 };
 
